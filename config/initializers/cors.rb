27615f271506464d4291c12affc0c25cbe4daaec
ippei-shimizu <ippei.shimizu.32@gmail.com>
2023-11-23 19:46:48 +0900

    🐛 Improve: CORS設定

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'localhost:8000', '127.0.0.1:8000','https://rails-api-nextjs-verification-app-front.vercel.app/'
+    origins 'localhost:8000', '127.0.0.1:8000','https://rails-api-nextjs-verification-app-front.vercel.app'
 
     resource "*",
       headers: :any,
